e52a920325ca92ded0394d17e1643b36fc36e272
kevinpenny3 <kevin.penny3@yahoo.com>
2020-01-21 14:50:41 -0600

    added friend search function

--- scripts/friends/Friends.js
+++ scripts/friends/Friends.js
@@ -2,8 +2,8 @@
 export const friendCardComponent = (friend) => {
   return `
   <section class="friendCard">
-    <h4 class="friendUsername">Username:${friend.user.email}</h4>
-    <div class="friendsStockPhoto"><img alt="imagePlaceholder" src=""></div>
+    <h4 class="friendUsername">${friend.user.email}</h4>
+    <div class="friendsStockPhoto"><img alt="imagePlaceholder" src="carnival.png"></div>
     <button id="deleteFriend--${friend.id}">Delete Friend</button>
     </section>
     `
--- scripts/friends/FriendsPreview.js
+++ scripts/friends/FriendsPreview.js
@@ -1,1 +1,10 @@
 // friendsPreview Card username, stock photo, and add friend button, add button makes a custom event   
+export const friendPreviewComponent = (friend) => {
+    return `
+    <section class="friendPreviewCard">
+      <h4 class="friendUsername">${friend.email}</h4>
+      <div class="friendsStockPhoto"><img alt="imagePlaceholder" src="carnival.png"></div>
+      <button id="addFriend--${friend.id}">Add Friend</button>
+      </section>
+      `
+    }
