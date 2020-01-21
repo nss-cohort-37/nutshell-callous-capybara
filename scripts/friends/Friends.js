2dd2da672cfd8dc51598e73563db183b5a36d854
namitamanohar <manohar.namita@gmail.com>
2020-01-21 09:19:23 -0600

    friend card

--- scripts/friends/Friends.js
+++ scripts/friends/Friends.js
@@ -1,1 +1,12 @@
 // username, stock image, delete button, 
+export const friendCardComponent = (friend) => {
+  return `
+  <section class="friendCard">
+    <h4 class="friendUsername">Title:${friend.user.email}</h4>
+    <div class="articleSynopsis">Synopsis:${friend.synopsis}</div>
+    <div class="articleUrl">Url:${friend.url}</div>
+    <button id="deleteFriend--${friend.id}">Edit Article</button>
+    </section>
+    `
+  }
+  
